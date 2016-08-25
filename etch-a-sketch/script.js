01687c5317b780def01bfad736d61bedc1b623f0
Enogwe Victor <victor.enogwe@gmail.com>
2016-08-25 19:19:00 +0100

    Update script.js

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -5,13 +5,19 @@ $(document).ready(function(){
     function userInput(){
         width = prompt("please enter a new sketch box width");
         height= prompt("please enter a new sketch box height");
+        numbersBoxes = 960 / width;
+        boxes = '<div class="box" style="width: ' + width + 'px,' + height: ' + height + 'px"></div>';
+        $("#divContainer").prepend(boxes.repeat(numbersBoxes));
     }
+    
+    var numbersBoxes = 960 / width;
+    var boxes = '<div class="box" style="width: 40px, height: 40px"></div>';
+    $("#divContainer").prepend(boxes.repeat(numbersBoxes));
+    
     $("#reset").on("click", function(e){
         e.preventDefault();
+        $(".box").remove();
         userInput();
     });
-    var numbersBoxes = 960 / width;
-    var boxes = '<div class="box" style="width: ' + width + ',' + height: ' + height + 'px"></div>';
-    $("#divContainer").prepend(boxes.repeat(numbersBoxes));
     
 });
